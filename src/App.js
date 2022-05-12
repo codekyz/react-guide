9a189ca8d2466f3706cc76a7d680f95fcd195b4e
codekyz <codekyz0111@gmail.com>
2022-05-12 11:48:14 +0900

    Finished Section 4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-function App() {
-  return <div></div>;
-}
+import Expenses from "./components/Expenses/Expenses";
+
+const App = () => {
+  const expenses = [
+    { title: "Car  Insurance", amount: 294.67, date: new Date(2021, 2, 28) },
+    { title: "Car  Insurance", amount: 294.67, date: new Date(2021, 2, 28) },
+    { title: "Car  Insurance", amount: 294.67, date: new Date(2021, 2, 28) },
+    { title: "Car  Insurance", amount: 294.67, date: new Date(2021, 2, 28) },
+  ];
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
+    </div>
+  );
+};
 
 export default App;
